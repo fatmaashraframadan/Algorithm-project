e6ab45399847b5ecd94e4392093bef016dc349d1
Mustafa badr <36304069+moustafaBadrr@users.noreply.github.com>
2020-05-29 19:43:43 +0200

    Update GraphPanel.java

--- src/Output/GraphPanel.java
+++ src/Output/GraphPanel.java
@@ -1,11 +1,10 @@
 package Output;
 
-
-
 import java.awt.Container;
 import java.awt.Dimension;
 import javax.swing.JFrame;
 
+import Graph.Graph;
 import edu.uci.ics.jung.algorithms.layout.CircleLayout;
 import edu.uci.ics.jung.graph.DirectedSparseGraph;
 import edu.uci.ics.jung.graph.SparseMultigraph;
@@ -20,16 +19,16 @@ public class GraphPanel extends Container
     PickedState<Number> pickedState = null;
     SparseMultigraph<Integer,String> euler = new SparseMultigraph();
 
-    public GraphPanel(Integer[][] nodes_list)
+    public GraphPanel(Graph graph1)
     {
         try
         {
             graph = new DirectedSparseGraph<Integer, Integer>();
-            construct_graph(nodes_list);
+            construct_graph(graph1);
 
             vv = new VisualizationViewer<Integer, Integer>
-                        (new CircleLayout<Integer, Integer>(graph), new Dimension(400, 400));
-            
+                    (new CircleLayout<Integer, Integer>(graph), new Dimension(400, 400));
+
 
             // The following code adds capability for mouse picking of
             // vertices/edges. Vertices can even be moved!
@@ -40,31 +39,23 @@ public class GraphPanel extends Container
             System.err.println("Caught Exception: " + e.getMessage());
         }
     }
-
-    /*Attach the graph panel/container to a specified frame*/
     public void attach_to_frame(JFrame frame)
     {
         frame.setContentPane(vv);
     }
 
-    /*This one should be reimplemented*/
-    private void construct_graph(Integer[][] nodes_list)
+
+    private void construct_graph(Graph graph1)
     {
-       
-        /*add the nodes*/
-        /*for(int i=0; i<nodes_list.length; i++)
-        {
-            graph.addVertex(i);
-          }*/
         int x=0;
-        for (int i = 0; i < nodes_list.length; i++) {
+        for (int i = 0; i < graph1.vertices.size(); i++) {
             euler.addVertex(i);
-			for (int j = 0; j < nodes_list.length; j++) {
-				if(nodes_list[i][j]==1) {
-					graph.addEdge(x,i, j);
-					x++;
-				}
-			}
-		}
+            for (int j = 0; j < graph1.vertices.size(); j++) {
+                if(graph1.representation[i][j]==1) {
+                    graph.addEdge(x,i, j);
+                    x++;
+                }
+            }
+        }
     }
 }
